666d745dceac1959c857ca423b05c7282e28c758
Fabio Yamashita <yamashitafabio@gmail.com>
2022-09-01 12:59:33 -0300

    Adding Tower Breakers Solution

---
+++ C#/Solutions/tower-breakers.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class tower_breakers_Result
+{
+
+    /*
+     * Complete the 'towerBreakers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     */
+
+    public static int towerBreakers(int n, int m)
+    {
+        return (n % 2 == 0 || m == 1) ? 2 : 1;
+    }
+
+}
+
+class tower_breakers_Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int m = Convert.ToInt32(firstMultipleInput[1]);
+
+            int result = tower_breakers_Result.towerBreakers(n, m);
+
+            Console.WriteLine(result);
+        }
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
+
+
