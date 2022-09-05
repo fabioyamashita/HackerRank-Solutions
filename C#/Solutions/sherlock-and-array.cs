64b3d32236e457ad8f6b06447313a48bd163d0e5
Fabio Yamashita <yamashitafabio@gmail.com>
2022-09-05 13:55:23 -0300

    Adding Sherlock and Array Solution

---
+++ C#/Solutions/sherlock-and-array.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+class balancedSumsResult
+{
+
+    /*
+     * Complete the 'balancedSums' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static string balancedSums(List<int> arr)
+    {
+        var leftSum = 0;
+        var rightSum = arr.Sum();
+
+        for (int i = 0; i < arr.Count; i++)
+        {
+            rightSum -= arr[i];
+
+            if (leftSum == rightSum) return "YES";
+
+            leftSum += arr[i];
+        }
+
+        return "NO";
+    }
+
+}
+
+class balancedSumsSolution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int T = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int TItr = 0; TItr < T; TItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            string result = balancedSumsResult.balancedSums(arr);
+
+            Console.WriteLine(result);
+        }
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
